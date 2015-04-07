a141c8d0fdd22eda4799981ddbf2165f2f2e6c64
garydickienson <gary.dickenson@icloud.com>
2015-04-07 14:20:38 -0500

    Initial creation

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,5 @@
+/**
+ * Created by GDICK on 4/7/2015.
+ */
+public class FizzBuzz {
+}
---
+++ src/test/java/FizzBuzz_UT.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by GDICK on 4/7/2015.
+ */
+public class FizzBuzz_UT {
+
+}
