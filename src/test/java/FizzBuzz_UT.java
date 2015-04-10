d65b8b074f99bf2330d006a39a0f64293987a4e3
garydickienson <gary.dickenson@icloud.com>
2015-04-09 21:02:28 -0500

    And pom.xml with Ash's help

--- src/test/java/FizzBuzz_UT.java
+++ src/test/java/FizzBuzz_UT.java
@@ -4,5 +4,7 @@ import static org.junit.Assert.*;
  * Created by GDICK on 4/7/2015.
  */
 public class FizzBuzz_UT {
+    @Test public void testTrue() { assertTrue(true); }
 
+    @Test(expectedException=???Exception.class) public void testTrueShouldFailForFalse() { assertTrue(false); }
 }
