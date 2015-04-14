802f7a4dfcfed5d3b159d304730fc8069262b055
G D <dicken.gary@yahoo.com>
2015-04-13 22:49:05 -0500

    incrementail work from home

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -4,26 +4,36 @@ import java.util.HashMap;
  * Created by Gary Dickenson on 4/7/2015.
  */
 public class FizzBuzz {
-    private static HashMap<Integer, String> outputWordList = new HashMap<>();
-//        h.put(1, 1000);
-//        h.put(20, 1001);
+    private static HashMap<Integer, String> divisor_Word_Map;
+    private int max_Len_Divisor_Word_list = 10;
+    private static HashMap<Integer, String> include_Word_Map;
+    private int max_Len_Include_Word_list = 5;
+    private Boolean output_On_Division = Boolean.TRUE;
+    private Boolean output_On_Include  = Boolean.FALSE;
+
     public FizzBuzz() {
-        outputWordList.put(3, "Fizz");
-        outputWordList.put(5, "Buzz");
+        divisor_Word_Map = new HashMap<Integer, String>();
+        divisor_Word_Map.put(  3, "Fizz");
+        divisor_Word_Map.put(  5, "Buzz");
+        divisor_Word_Map.put(3*5, "Fizzbuzz");
+
+        include_Word_Map = new HashMap<Integer, String>();
+        include_Word_Map.put(2, "Pip");
+        include_Word_Map.put(5, "Pop");
     }
 
     public String evaluate(int inputNumber, int[] divisors) {
         String returnStr = String.valueOf(inputNumber);
         if(inputNumber < 3) return returnStr;
         if(divisors.length < 1 ) return returnStr;
-        String delimitor = "";
+        String delimiter = "";
         returnStr = "";
         for (int  i : divisors) {
             System.out.println("Divisor is : " + i);
             if(0 < i){
                 if(0 == (inputNumber % i)){
-                    returnStr += delimitor + outputWordList.get(i);
-                    delimitor = "-";
+                    returnStr += delimiter + divisor_Word_Map.get(i);
+                    delimiter = "-";
                 }
             }
         }
--- src/test/java/FizzBuzz_UT.java
+++ src/test/java/FizzBuzz_UT.java
@@ -12,11 +12,20 @@ public class FizzBuzz_UT {
 //    @Test(expected =Exception.class) public void testTrueShouldFailForFalse() {assertTrue(false); }
 
     @Test
-    public void ableToCreateObject(){
+    public void able_To_Create_Default(){
+        System.out.println("Able to create, no parameter constructor");
         FizzBuzz fizzBuzz = new FizzBuzz();
-        //System.out.println("Able to create");
 
     }
+    @Test
+    public void able_To_Create_Parametrized(){
+        System.out.println("Able to create, parametrized constructor");
+        FizzBuzz fizzBuzz = new FizzBuzz();
+    }
+    @Test public void able_To_Replace_Divisor_Word_List(){ }
+    @Test public void able_To_Add_Divisor_Word_List_Item(){ }
+    @Test public void able_To_Remove_Divisor_Word_List_Item(){ }
+    @Test public void able_To_Replace_Divisor_Word_List_Item(){ }
 /*
     get Word list
     get in int in word list hash keys
@@ -25,6 +34,11 @@ public class FizzBuzz_UT {
     test if maximum items set
     report number of additional divisors that can be set.
     test for zero, neg numbers, number 1
+
+    Design question how to deal with multiples?
+    Do I require the client to set them,
+    automatically create them and allow over-ride?
+    Do automatically generated ones count against the max?
 
 
     */
